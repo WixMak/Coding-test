a6450a5ca6fc9b849587a165295ddc14a5c432f5
WixMak <88415615+WixMak@users.noreply.github.com>
2023-09-21 22:05:49 +1000

    Install Redux Devtools extension

--- frontend/src/redux-saga/store.js
+++ frontend/src/redux-saga/store.js
@@ -2,8 +2,12 @@ import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import Reducer from "./reducer";
 import Saga from "./sagas";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 //Saga middleware setup
 const sagaMiddleware = createSagaMiddleware();
-export const store = createStore(Reducer, applyMiddleware(sagaMiddleware));
+export const store = createStore(
+    Reducer,
+    composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 sagaMiddleware.run(Saga);
